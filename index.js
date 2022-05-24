c56d3c4cb9fd762000d7e8346c92466e5a87dc38
unknown <rhrabin934602@gmail.com>
2022-05-24 09:42:52 +0600

    get order and delete order api created

--- index.js
+++ index.js
@@ -19,17 +19,77 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("electroma").collection("services");
+        const orderCollection = client.db("electroma").collection("orders");
+
+
+        //----------------------------------------------------//
+        //----------------------------------------------------//
+        //Service Collection//
 
         app.get('/services', async (req, res) => {
             const result = await serviceCollection.find().toArray();
             res.send(result)
         })
+
+        //update service quantity api
+        app.put('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const quantity = req.body.quantity;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+
+            const updateDoc = {
+                $set: {
+                    quantity: quantity
+                }
+            }
+            const result = await serviceCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
         // load single service by id
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id)
             const query = { _id: ObjectId(id) };
             const result = await serviceCollection.findOne(query);
+            res.send(result)
+        })
+
+
+
+        //------------------------------------------------------------------//
+        //------------------------------------------------------------------//
+        // Order collection//
+
+
+        // save order from user , create post api
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
+
+        })
+
+        // get all order for user by email
+        app.get('/order/:email', async (req, res) => {
+            const email = req.params.email;
+            const result = await orderCollection.find({ email: email }).toArray();
+            res.send(result)
+        })
+
+        // delete Order api
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        //get single order by id
+        app.get('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.findOne(query).toArray();
             res.send(result)
         })
     }
