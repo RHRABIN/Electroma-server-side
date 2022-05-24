63633ed90e19adf3dda3ec4c417ad621a71311ed
unknown <rhrabin934602@gmail.com>
2022-05-24 16:43:48 +0600

    order updated and pament api completed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
-
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 //middleware 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +20,7 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db("electroma").collection("services");
         const orderCollection = client.db("electroma").collection("orders");
+        const paymentCollection = client.db("electroma").collection("payments");
 
 
         //----------------------------------------------------//
@@ -30,7 +31,19 @@ async function run() {
             const result = await serviceCollection.find().toArray();
             res.send(result)
         })
-
+        // get payment
+        app.post('/create-payment-intent', async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            console.log(price)
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
         //update service quantity api
         app.put('/services/:id', async (req, res) => {
             const id = req.params.id;
@@ -60,6 +73,23 @@ async function run() {
         //------------------------------------------------------------------//
         // Order collection//
 
+        //-----------------------//
+        //transaction api
+        app.patch('/orderbooking/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId,
+
+                }
+            }
+            const updateOrder = await orderCollection.updateOne(filter, updateDoc);
+            const paymentSummery = await paymentCollection.insertOne(payment);
+            res.send(updateOrder)
+        })
 
         // save order from user , create post api
         app.post('/order', async (req, res) => {
@@ -79,17 +109,17 @@ async function run() {
         // delete Order api
         app.delete('/order/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id)
+
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
             res.send(result)
         })
 
         //get single order by id
-        app.get('/order/:id', async (req, res) => {
+        app.get('/item/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await orderCollection.findOne(query).toArray();
+            const query = { _id: ObjectId(id) }
+            const result = await orderCollection.findOne(query);
             res.send(result)
         })
     }
