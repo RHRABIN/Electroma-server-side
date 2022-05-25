7654e3313179cdcabd405d339ed02ed022ae4c63
unknown <rhrabin934602@gmail.com>
2022-05-25 16:46:25 +0600

    create admint api

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 //middleware 
 app.use(cors());
 app.use(express.json());
+
+// jwrverify function
+function jwtVerify(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.APP_JWT, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden' })
+        }
+        req.decoded = decoded;
+        console.log(decoded)
+        next();
+    })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -21,6 +41,8 @@ async function run() {
         const serviceCollection = client.db("electroma").collection("services");
         const orderCollection = client.db("electroma").collection("orders");
         const paymentCollection = client.db("electroma").collection("payments");
+        const reviewCollection = client.db("electroma").collection("reviews");
+        const userCollection = client.db("electroma").collection("users");
 
 
         //----------------------------------------------------//
@@ -122,6 +144,72 @@ async function run() {
             const result = await orderCollection.findOne(query);
             res.send(result)
         })
+
+        //----------------------------------------------------------//
+        //----------------------------------------------------------//
+        //review collection//
+        app.get('/review', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result)
+        })
+        app.post('/review', async (req, res) => {
+            const comment = req.body;
+            const result = await reviewCollection.insertOne(comment);
+            res.send(result)
+        })
+
+
+
+        //--------------------------------------------------------------//
+        //---------------------------------------------------------------//
+        // user collection
+
+        //make admin api
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        // add user in database
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const userInfo = req.body;
+
+            const filter = { email: userInfo.email }
+            const option = { upsert: true };
+            const updateDoc = {
+                $set: userInfo,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, option);
+            const token = jwt.sign({ email: email }, process.env.APP_JWT, { expiresIn: '1h' });
+            res.send({ result, token });
+        })
+        // update user information
+        app.patch('/user/update/:email', async (req, res) => {
+            const email = req.params.email;
+            const updateInfo = req.body.userProfileInfo;
+            const filter = { email: email };
+
+            const updateDoc = {
+                $set: {
+                    name: updateInfo.name,
+                    address: updateInfo.address,
+                    phone: updateInfo.phone,
+                    education: updateInfo.education,
+                    linkdin: updateInfo.linkdin
+                }
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
     }
     finally {
 
